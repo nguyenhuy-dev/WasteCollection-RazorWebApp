6339696973a3f7f16bd6667d5fd20bde5bda5cb5
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2026-02-03 10:58:54 +0700

    [feat] add signalR feature

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs;
+
+public class ChatHub(ILogger<ChatHub> logger) : Hub
+{
+    private readonly ILogger<ChatHub> _logger = logger;
+
+    public async Task SendMessage(string user, string message)
+    {
+        if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(message))
+        {
+            _logger.LogWarning("User must be identified and message cannot be empty");
+            return;
+        }
+        
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
