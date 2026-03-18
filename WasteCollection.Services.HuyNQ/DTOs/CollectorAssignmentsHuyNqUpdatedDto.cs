92b8dfc0386967bc041abdf901cce19e7f5f2540
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2026-03-18 08:09:18 +0700

    [feat] add validaion and detail with realtime SignalR

--- WasteCollection.Services.HuyNQ/DTOs/CollectorAssignmentsHuyNqUpdatedDto.cs
+++ WasteCollection.Services.HuyNQ/DTOs/CollectorAssignmentsHuyNqUpdatedDto.cs
@@ -1,4 +1,6 @@
-﻿namespace WasteCollection.Services.HuyNQ.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WasteCollection.Services.HuyNQ.DTOs;
 
 public class CollectorAssignmentsHuyNqUpdatedDto
 {
@@ -10,14 +12,19 @@ public class CollectorAssignmentsHuyNqUpdatedDto
 
     public string Status { get; set; } = default!;
 
+    [Required]
     public DateTime? ArrivalTime { get; set; }
 
+    [Required]
     public DateTime? CompletionTime { get; set; }
 
+    [Required]
+    [Range(0, 100000, ErrorMessage = "CollectedWeight must be >= 0.")]
     public decimal? CollectedWeight { get; set; }
 
     public string ProofImageUrl { get; set; } = default!;
 
+    [Required]
     public DateTime? EstimatedArrivalTime { get; set; }
 
     public string Notes { get; set; } = default!;
